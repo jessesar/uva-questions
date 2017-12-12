d89ff6e83fea4d227b7092f4237c6434f8355c3b
Jesse van der Sar <jesse@sar.nl>
2017-12-12 01:17:52 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='questions',
+    version='1.0',
+    description='UvA questions module',
+    packages=['questions'],
+    author='Jesse van der Sar',
+    author_email='j.d.vandersar@uva.nl',
+)
